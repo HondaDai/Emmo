d7883082f8047f786b69e621501638d16e5b8921
HondaDai <hondadai.tw@gmail.com>
2013-12-06 12:30:30 +0800

    initial Emmo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emmo::Application.config.secret_key_base = 'c961800f93edc17633849608b2b206e1a1edc95b1a0b8dafb2cecc1d8df588c846cb6a25d55bcbbf650212cab1e8e70151a90843b12061c5d79af3c26ebf8701'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Emmo::Application.config.session_store :cookie_store, key: '_Emmo_session'
