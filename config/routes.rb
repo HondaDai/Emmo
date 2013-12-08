657a6de05e79d9421ababd1d37fb4f284da47d1b
HondaDai <hondadai.tw@gmail.com>
2013-12-08 13:29:06 +0800

    add User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Emmo::Application.routes.draw do
+  get "user/login"
+  get "user/register"
   get "static_pages/home"
   get "static_pages/help"
 
 
-  root "static_pages#home"
+  root "user#login"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
