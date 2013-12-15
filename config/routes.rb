ffe8f7b57908f42c2d7b7bccf6f61937b40b7525
HondaDai <hondadai.tw@gmail.com>
2013-12-15 19:51:53 +0800

    split into sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Emmo::Application.routes.draw do
 
   resources :users 
   resources :sessions 
+  resources :books
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
